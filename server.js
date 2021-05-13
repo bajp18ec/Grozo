5d047cd128ecd2304fc2b55f0220a9adc8644345
Basavaraju JP <raj118jp@gmail.com>
2021-05-13 04:53:00 -0700

    make home page

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+
+const port = process.env.PORT || 3300
+
+app.use(expressLayout)
+app.set('views',path.join(__dirname,'/resources/views'))
+// app.set('layout','./Layouts/full-width')
+app.set('view engine','ejs')
+
+//assets
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.render('home')
+    // res.send('hello world')
+})
+app.get('/about',(req,res)=>{
+    res.render('index')
+    // res.send('hello world')
+})
+
+app.listen(port,(req,res)=>{
+    console.log(`listinig on port ${port}`)    
+})
